976905abc92da620f76236adab4be6fde4a71abc
Arsham1024 <Arsham512@gmail.com>
2021-09-07 20:14:00 -0700

    completed a challenge

--- hidden_message.py
+++ hidden_message.py
@@ -2,3 +2,20 @@ import stack
 
 # first stacks is the module and second is the class
 s = stack.Stack()
+
+string = "This is the first question to solve using stakc. The objective is to reverse this string."
+reversed_string = ""
+
+for i in string:
+    s.push(i)
+
+while not(s.is_empty()):
+    reversed_string = reversed_string + s.pop()
+
+# Here is the reversed string
+print("the reversed string is: ")
+print(reversed_string)
+
+
+
+
